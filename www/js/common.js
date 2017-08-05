a4cbdad185de1e432357a03f93f2a9fef5253370
Dmitry Turovtsov <web.best.master@gmail.com>
2017-08-05 18:51:52 +0300

    in progress

--- www/js/common.js
+++ www/js/common.js
@@ -1,7 +1,6 @@
 import {countTo} from './lib/util';
 require('./lib/main-model');
 require('pixi.js');
-require('gsap');
 
 countTo(75, ii => require(`base64-image-loader!./../base64/hug-${ii}.png`));
 
